a23a9ed5949f2ec8aba506b38f82488520a869e4
Prasad Malvi <prasadmalvi23@gmail.com>
2025-01-30 11:13:54 +0530

    Finished JS pop up handle

--- src/main/java/GlowTest/JSPopUpHandle.java
+++ src/main/java/GlowTest/JSPopUpHandle.java
@@ -1,10 +1,57 @@
 package GlowTest;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class JSPopUpHandle {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-
+		//JS ALERTS:
+		//1.alert
+		//2.confirm
+		//3.promtb 
+		
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://testpages.herokuapp.com/styled/alerts/alert-test.html");
+		
+		//alert click okey
+//		driver.findElement(By.xpath("//input[@id='alertexamples']")).click();
+//		Thread.sleep(3000);
+//		
+//		Alert alert = driver.switchTo().alert();
+//		
+//		String text = alert.getText();
+//		System.out.println(text);
+//		alert.accept();
+//	alert.dismiss();
+		
+		//Confirm Click
+//		driver.findElement(By.xpath("//input[@id='confirmexample']")).click();
+//		Thread.sleep(3000);
+//		
+//		Alert alert = driver.switchTo().alert();
+//		
+//		String text = alert.getText();
+//		System.out.println(text);
+////		alert.accept();
+//		alert.dismiss();
+		
+		
+		//Prompt 
+		driver.findElement(By.xpath("//input[@id='promptexample']")).click();
+		Thread.sleep(3000);
+		
+		Alert alert = driver.switchTo().alert();
+		
+		String text = alert.getText();
+		System.out.println(text);
+		alert.sendKeys("Prasad");
+		alert.accept();
+//		alert.dismiss();
 	}
 
 }
